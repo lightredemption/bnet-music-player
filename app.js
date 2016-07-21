a1f6d0263ff4a489dbe1f8cfd696bbb24f795d84
vunt <vunt@mail.uc.edu>
2016-07-20 20:05:53 -0400

    added shuffle

--- app.js
+++ app.js
@@ -1,28 +1,87 @@
 var app = angular.module('bnet-music-player', [`ngAudio`, `720kb.background`]);
-app.controller(`BaseController`, [`$scope`, `ngAudio`, `$http`, ($scope, ngAudio, $http) => {
-
-  $scope.nowPlaying = false;
-
-  $http.get(`music.json`)
-  .then((res) => {
-    $scope.music = res.data;
-    $scope.current = $scope.music[0];
-    $scope.load();
-  });
-
-  $scope.load = () => {
-    $scope.song = ngAudio.load($scope.music[0].url);
+app.filter('secondsToDateTime', [function() {
+  return function(seconds) {
+    return new Date(1970, 0, 1).setSeconds(seconds);
   };
+}])
+app.controller(`BaseController`, [`$scope`, `ngAudio`, `$http`, `$interval`,
+  ($scope, ngAudio, $http, $interval) => {
 
-  $scope.backgroundUrl = `public/img/sc.jpg`;
+    $scope.nowPlaying = true;
+    $scope.playImg = `public/img/Pause.png`;
 
-  $scope.play = () => {
-    if ($scope.nowPlaying) {
-      $scope.song.pause();
-    } else {
+    $http.get(`music.json`)
+    .then((res) => {
+      $scope.music = res.data;
+      $scope.current = $scope.music[0];
+      $scope.position = 0;
+      $scope.load();
+    });
+
+    $scope.load = () => {
+      $scope.song = ngAudio.load($scope.current.url);
       $scope.song.play();
-    }
-    $scope.nowPlaying = !$scope.nowPlaying;
-  };
+    };
 
-}]);
+    $scope.backgroundUrl = `public/img/sc.jpg`;
+
+    $scope.play = () => {
+      if ($scope.nowPlaying) {
+        $scope.song.pause();
+        $scope.playImg = `public/img/Play.png`;
+      } else {
+        $scope.song.play();
+        $scope.playImg = `public/img/Pause.png`;
+      }
+      $scope.nowPlaying = !$scope.nowPlaying;
+    };
+
+    $scope.next = () => {
+      $scope.song.stop();
+      if ($scope.shuffle) {
+        let point = Math.floor(Math.random() * $scope.music.length);
+        while (point === $scope.position) {
+          point = Math.floor(Math.random() * $scope.music.length);
+        }
+        $scope.position = point;
+      }
+      if (!$scope.shuffle && !$scope.repeat) {
+        if (($scope.position + 1) ===  $scope.music.length) {
+          $scope.position = 0;
+        } else {
+          $scope.position = $scope.position+=1;
+        }
+      }
+
+      $scope.current = $scope.music[$scope.position];
+      $scope.load();
+    };
+
+    $scope.prev = () => {
+      $scope.song.stop();
+      if ($scope.position === 0) {
+        $scope.position = $scope.music.length - 1;
+      } else {
+        $scope.position = $scope.position-=1;
+      }
+      $scope.current = $scope.music[$scope.position];
+      $scope.load();
+    };
+
+    $scope.togShuffle = () => {
+      $scope.shuffle = !$scope.shuffle;
+      $scope.repeat = false;
+    };
+
+    $scope.togRepeat = () => {
+      $scope.repeat = !$scope.repeat;
+      $scope.shuffle = false;
+    };
+
+    $interval(() => {
+      if ($scope.song.remaining < 1) {
+        $scope.next();
+      }
+    }, 500)
+
+  }]);
--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,26 @@
 'use strict';
 
 var app = angular.module('bnet-music-player', ['ngAudio', '720kb.background']);
-app.controller('BaseController', ['$scope', 'ngAudio', '$http', function ($scope, ngAudio, $http) {
+app.filter('secondsToDateTime', [function () {
+  return function (seconds) {
+    return new Date(1970, 0, 1).setSeconds(seconds);
+  };
+}]);
+app.controller('BaseController', ['$scope', 'ngAudio', '$http', '$interval', function ($scope, ngAudio, $http, $interval) {
 
-  $scope.nowPlaying = false;
+  $scope.nowPlaying = true;
+  $scope.playImg = 'public/img/Pause.png';
 
   $http.get('music.json').then(function (res) {
     $scope.music = res.data;
     $scope.current = $scope.music[0];
+    $scope.position = 0;
     $scope.load();
   });
 
   $scope.load = function () {
-    $scope.song = ngAudio.load($scope.music[0].url);
+    $scope.song = ngAudio.load($scope.current.url);
+    $scope.song.play();
   };
 
   $scope.backgroundUrl = 'public/img/sc.jpg';
@@ -20,9 +28,59 @@ app.controller('BaseController', ['$scope', 'ngAudio', '$http', function ($scope
   $scope.play = function () {
     if ($scope.nowPlaying) {
       $scope.song.pause();
+      $scope.playImg = 'public/img/Play.png';
     } else {
       $scope.song.play();
+      $scope.playImg = 'public/img/Pause.png';
     }
     $scope.nowPlaying = !$scope.nowPlaying;
   };
+
+  $scope.next = function () {
+    $scope.song.stop();
+    if ($scope.shuffle) {
+      var point = Math.floor(Math.random() * $scope.music.length);
+      while (point === $scope.position) {
+        point = Math.floor(Math.random() * $scope.music.length);
+      }
+      $scope.position = point;
+    }
+    if (!$scope.shuffle && !$scope.repeat) {
+      if ($scope.position + 1 === $scope.music.length) {
+        $scope.position = 0;
+      } else {
+        $scope.position = $scope.position += 1;
+      }
+    }
+
+    $scope.current = $scope.music[$scope.position];
+    $scope.load();
+  };
+
+  $scope.prev = function () {
+    $scope.song.stop();
+    if ($scope.position === 0) {
+      $scope.position = $scope.music.length - 1;
+    } else {
+      $scope.position = $scope.position -= 1;
+    }
+    $scope.current = $scope.music[$scope.position];
+    $scope.load();
+  };
+
+  $scope.togShuffle = function () {
+    $scope.shuffle = !$scope.shuffle;
+    $scope.repeat = false;
+  };
+
+  $scope.togRepeat = function () {
+    $scope.repeat = !$scope.repeat;
+    $scope.shuffle = false;
+  };
+
+  $interval(function () {
+    if ($scope.song.remaining < 1) {
+      $scope.next();
+    }
+  }, 500);
 }]);
